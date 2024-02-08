dc7d4107ac92729376769adabc1a7b70360359b4
Leonardo Rivera <leorivera_88@hotmail.com>
2024-02-08 12:56:09 -0500

    pong
    Co-authored-by: Anders Richardsson <2107110+justincorrigible@users.noreply.github.com>

--- apps/server/routers/ping.ts
+++ apps/server/routers/ping.ts
@@ -2,6 +2,6 @@ import { Router } from 'express';
 
 const router = Router();
 
-router.get('/', (_req, res) => res.send({ message: 'Ping' }));
+router.get('/', (_req, res) => res.send({ message: 'Pong' }));
 
 export default router;
