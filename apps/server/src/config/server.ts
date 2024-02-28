96734397f291e20ceb11685f4177ce3f72217348
Leonardo Rivera <leorivera_88@hotmail.com>
2024-02-28 14:37:42 -0500

    remove DEBUG_MODE
    remove DEBUG_MODE environment variable, using LOG_LEVEL to control the level

--- apps/server/src/config/server.ts
+++ apps/server/src/config/server.ts
@@ -5,7 +5,6 @@ import { AppConfig } from 'common';
 export const getServerConfig = () => {
 	return {
 		port: process.env.PORT || 3030,
-		debug_mode: process.env.DEBUG || false,
 		upload_limit: process.env.UPLOAD_LIMIT || '50mb',
 	};
 };
