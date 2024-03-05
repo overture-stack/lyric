7b50368e5ee448d45c19aca83fa8aa2820fd6d16
Leonardo Rivera <leorivera_88@hotmail.com>
2024-03-05 10:56:31 -0500

    required environment variables

--- apps/server/src/config/server.ts
+++ apps/server/src/config/server.ts
@@ -9,16 +9,24 @@ export const getServerConfig = () => {
 	};
 };
 
+const getRequiredConfig = (name: string) => {
+	const value = process.env[name];
+	if (!value) {
+		throw new Error(`No Environment Variable provided for required configuration parameter '${name}'`);
+	}
+	return value;
+};
+
 export const defaultAppConfig: AppConfig = {
 	db: {
-		host: process.env.DB_URL || '',
-		port: Number(process.env.DB_PORT) || 5432,
-		database: process.env.DB_NAME || '',
-		user: process.env.DB_USER || '',
-		password: process.env.DB_PASSWORD || '',
+		host: getRequiredConfig('DB_URL'),
+		port: Number(getRequiredConfig('DB_PORT')),
+		database: getRequiredConfig('DB_NAME'),
+		user: getRequiredConfig('DB_USER'),
+		password: getRequiredConfig('DB_PASSWORD'),
 	},
 	schemaService: {
-		url: process.env.LECTERN_URL || '',
+		url: getRequiredConfig('LECTERN_URL'),
 	},
 	logger: {
 		level: process.env.LOG_LEVEL || 'info',
