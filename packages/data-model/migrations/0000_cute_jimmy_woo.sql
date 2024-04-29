9e8f71b68d54a4eed53a4c693cca965eaa56cdfb
Leonardo Rivera <leorivera_88@hotmail.com>
2024-04-29 17:18:38 -0400

    new migrations

--- packages/data-model/migrations/0000_cute_jimmy_woo.sql
+++ packages/data-model/migrations/0000_cute_jimmy_woo.sql
@@ -1,10 +1,59 @@
-ALTER TABLE "dictionary_categories" RENAME COLUMN "udpated_at" TO "updated_at";--> statement-breakpoint
-ALTER TABLE "submissions" RENAME COLUMN "updatedBy" TO "updated_by";--> statement-breakpoint
-ALTER TABLE "dictionary_categories" DROP CONSTRAINT "dictionary_categories_active_dictionary_id_dictionaries_id_fk";
+DO $$ BEGIN
+ CREATE TYPE "submission_state" AS ENUM('OPEN', 'VALID', 'INVALID', 'CLOSED', 'COMMITTED');
+EXCEPTION
+ WHEN duplicate_object THEN null;
+END $$;
 --> statement-breakpoint
-ALTER TABLE "dictionary_categories" ADD COLUMN "created_by" varchar;--> statement-breakpoint
-ALTER TABLE "dictionary_categories" ADD COLUMN "updated_by" varchar;--> statement-breakpoint
-ALTER TABLE "submitted_data" ADD COLUMN "updated_by" varchar;--> statement-breakpoint
+CREATE TABLE IF NOT EXISTS "dictionaries" (
+	"id" serial PRIMARY KEY NOT NULL,
+	"dictionary" jsonb NOT NULL,
+	"dictionary_category_id" integer,
+	"name" varchar NOT NULL,
+	"version" varchar NOT NULL,
+	"created_at" timestamp DEFAULT now(),
+	"created_by" varchar
+);
+--> statement-breakpoint
+CREATE TABLE IF NOT EXISTS "dictionary_categories" (
+	"id" serial PRIMARY KEY NOT NULL,
+	"active_dictionary_id" integer,
+	"name" varchar NOT NULL,
+	"created_at" timestamp DEFAULT now(),
+	"created_by" varchar,
+	"updated_at" timestamp,
+	"updated_by" varchar,
+	CONSTRAINT "dictionary_categories_name_unique" UNIQUE("name")
+);
+--> statement-breakpoint
+CREATE TABLE IF NOT EXISTS "submissions" (
+	"id" serial PRIMARY KEY NOT NULL,
+	"data" jsonb NOT NULL,
+	"dictionary_category_id" integer,
+	"dictionary_id" integer,
+	"errors" jsonb,
+	"organization" varchar NOT NULL,
+	"state" "submission_state",
+	"created_at" timestamp DEFAULT now(),
+	"created_by" varchar,
+	"updated_at" timestamp DEFAULT now(),
+	"updated_by" varchar
+);
+--> statement-breakpoint
+CREATE TABLE IF NOT EXISTS "submitted_data" (
+	"id" serial PRIMARY KEY NOT NULL,
+	"data" jsonb,
+	"dictionary_category_id" integer,
+	"entity_name" varchar NOT NULL,
+	"is_valid" boolean,
+	"last_valid_schema_id" integer,
+	"organization" varchar NOT NULL,
+	"original_schema_id" integer,
+	"created_at" timestamp DEFAULT now(),
+	"created_by" varchar,
+	"updated_at" timestamp DEFAULT now(),
+	"updated_by" varchar
+);
+--> statement-breakpoint
 DO $$ BEGIN
  ALTER TABLE "dictionaries" ADD CONSTRAINT "dictionaries_dictionary_category_id_dictionary_categories_id_fk" FOREIGN KEY ("dictionary_category_id") REFERENCES "dictionary_categories"("id") ON DELETE no action ON UPDATE no action;
 EXCEPTION
--- packages/data-model/migrations/0007_yellow_shinobi_shaw.sql
+++
@@ -1,1 +0,0 @@
-ALTER TABLE "dictionaries" ALTER COLUMN "dictionary" SET NOT NULL;
