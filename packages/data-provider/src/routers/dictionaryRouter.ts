05056fb1c5cd1ed7d38edc253f7bace718734f7e
Leonardo Rivera <leorivera_88@hotmail.com>
2024-06-24 17:04:05 -0400

    remove notimplemented function and renaming variables

--- packages/data-provider/src/routers/dictionaryRouter.ts
+++ packages/data-provider/src/routers/dictionaryRouter.ts
@@ -3,14 +3,14 @@ import { Router, json, urlencoded } from 'express';
 import { auth } from '../middleware/auth.js';
 
 import { BaseDependencies } from '../config/config.js';
-import dictionaryControllers from '../controllers/dictionaryController.js';
+import dictionaryController from '../controllers/dictionaryController.js';
 
 const router = (dependencies: BaseDependencies): Router => {
 	const router = Router();
 	router.use(urlencoded({ extended: false }));
 	router.use(json());
 
-	router.post('/register', auth, dictionaryControllers(dependencies).registerDictionary);
+	router.post('/register', auth, dictionaryController(dependencies).registerDictionary);
 	return router;
 };
 
