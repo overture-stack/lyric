3c91bfceb9d52b234a5d387a4a224fc8cc32aefc
Jon Eubank <joneubank@gmail.com>
2026-02-28 13:33:57 -0500

    Move existing data-provider tests to test/unit

---
+++ packages/data-provider/test/unit/utils/submission/parseSubmissionSummaryResponse.spec.ts
@@ -0,0 +1,104 @@
+import { expect } from 'chai';
+import { describe, it } from 'mocha';
+
+import { createSubmissionSummaryResponse } from '../../../../src/utils/submissionUtils.js';
+import { SUBMISSION_STATUS, type SubmissionDataSummaryRepositoryRecord } from '../../../../src/utils/types.js';
+
+describe('Submission Utils - Parse a Submission object to a Summary of the Active Submission', () => {
+	const todaysDate = new Date();
+	it('should return a Summary without any data ', () => {
+		const submissionDataSummaryRepositoryRecord: SubmissionDataSummaryRepositoryRecord = {
+			id: 4,
+			data: {},
+			dictionary: { name: 'books', version: '1' },
+			dictionaryCategory: { name: 'favorite books', id: 1 },
+			errors: null,
+			organization: 'oicr',
+			status: SUBMISSION_STATUS.VALID,
+			createdAt: todaysDate,
+			createdBy: 'me',
+			updatedAt: null,
+			updatedBy: null,
+		};
+		const response = createSubmissionSummaryResponse(submissionDataSummaryRepositoryRecord);
+		expect(response).to.eql({
+			id: 4,
+			data: {
+				total: 0,
+			},
+			dictionary: { name: 'books', version: '1' },
+			dictionaryCategory: { name: 'favorite books', id: 1 },
+			errors: { total: 0 },
+			organization: 'oicr',
+			status: SUBMISSION_STATUS.VALID,
+			createdAt: todaysDate.toISOString(),
+			createdBy: 'me',
+			updatedAt: '',
+			updatedBy: '',
+		});
+	});
+	it('should return a Summary with insert, update and delete data ', () => {
+		const submissionDataSummaryRepositoryRecord: SubmissionDataSummaryRepositoryRecord = {
+			id: 3,
+			data: {
+				inserts: {
+					books: {
+						batchName: 'books.tsv',
+						recordsCount: 1,
+					},
+				},
+				updates: {
+					books: {
+						recordsCount: 1,
+					},
+				},
+				deletes: {
+					books: {
+						recordsCount: 1,
+					},
+				},
+			},
+			dictionary: { name: 'books', version: '1' },
+			dictionaryCategory: { name: 'favorite books', id: 1 },
+			errors: {},
+			organization: 'oicr',
+			status: SUBMISSION_STATUS.VALID,
+			createdAt: todaysDate,
+			createdBy: 'me',
+			updatedAt: null,
+			updatedBy: null,
+		};
+		const response = createSubmissionSummaryResponse(submissionDataSummaryRepositoryRecord);
+		expect(response).to.eql({
+			id: 3,
+			data: {
+				total: 3,
+				inserts: {
+					books: {
+						batchName: 'books.tsv',
+						recordsCount: 1,
+					},
+				},
+				updates: {
+					books: {
+						recordsCount: 1,
+					},
+				},
+				deletes: {
+					books: {
+						recordsCount: 1,
+					},
+				},
+			},
+			dictionary: { name: 'books', version: '1' },
+			dictionaryCategory: { name: 'favorite books', id: 1 },
+			errors: { total: 0 },
+			organization: 'oicr',
+			status: SUBMISSION_STATUS.VALID,
+			createdAt: todaysDate.toISOString(),
+			createdBy: 'me',
+			updatedAt: '',
+			updatedBy: '',
+		});
+	});
+});
