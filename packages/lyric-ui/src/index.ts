1f96358c9cb14621ef0bc100481fc436156039e9
James Lopez <jamestlopez.code@gmail.com>
2026-08-06 08:47:17 -0400

    feat: rename button

--- packages/lyric-ui/src/index.ts
+++ packages/lyric-ui/src/index.ts
@@ -1,4 +1,4 @@
 import './styles/globals.css';
 
-export { Button, type ButtonProps, buttonVariants } from './components/button/button';
+export { Button, type ButtonProps, buttonVariants } from './components/button/Button';
 export { cn } from './lib/utils';
