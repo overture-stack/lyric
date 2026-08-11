b5084e847f1ca35cba2a7f25fc7cfb5e0c632576
Leonardo Rivera <leorivera_88@hotmail.com>
2026-08-11 13:42:09 -0400

    filter submission records by action Type

--- packages/data-provider/src/utils/submissionResponseParser.ts
+++ packages/data-provider/src/utils/submissionResponseParser.ts
@@ -1,48 +1,78 @@
+import * as _ from 'lodash-es';
+
 import type { RecordsSummaryRepository } from '../repository/submissionRecordsRepository.js';
-import { type SubmissionDataSummaryWithTotal } from './types.js';
+import {
+	type DataDeletesSubmissionSummary,
+	type DataInsertsSubmissionSummary,
+	type DataUpdatesSubmissionSummary,
+	type SubmissionDataSummaryWithTotal,
+	type SubmissionSummary,
+	type SubmissionSummaryResponse,
+} from './types.js';
 
 // This function accepts a raw array of submission records from the database and builds a summary response.
 export const buildDataSummary = (rows: RecordsSummaryRepository[]): SubmissionDataSummaryWithTotal => {
-	const initialSummary: SubmissionDataSummaryWithTotal = {
-		inserts: {},
-		updates: {},
-		deletes: {},
-		totalRecords: 0,
-		errors: 0,
-	};
+	const inserts: Record<string, DataInsertsSubmissionSummary[]> = {};
+	const updates: Record<string, DataUpdatesSubmissionSummary[]> = {};
+	const deletes: Record<string, DataDeletesSubmissionSummary> = {};
+	let totalRecords = 0;
+	let errors = 0;
 
-	return rows.reduce((acc, row) => {
-		const { actionType, entityName, totalRecords, batchName, errors } = row;
-		const summaryItem = { batchName: batchName ?? '', recordsCount: totalRecords, errors };
+	for (const row of rows) {
+		const { actionType, entityName, totalRecords: rowTotalRecords, batchName, errors: rowErrors } = row;
+		const summaryItem = { batchName: batchName ?? '', recordsCount: rowTotalRecords, errors: rowErrors };
 
-		acc.totalRecords += totalRecords;
-		acc.errors += errors;
+		totalRecords += rowTotalRecords;
+		errors += rowErrors;
 
 		switch (actionType) {
 			case 'INSERT': {
-				const entitySummaries = acc.inserts ?? {};
-				entitySummaries[entityName] = entitySummaries[entityName] ?? [];
-				entitySummaries[entityName].push(summaryItem);
-				acc.inserts = entitySummaries;
+				const entitySummaries = inserts[entityName] ?? [];
+				entitySummaries.push(summaryItem);
+				inserts[entityName] = entitySummaries;
 				break;
 			}
 			case 'UPDATE': {
-				const entitySummaries = acc.updates ?? {};
-				entitySummaries[entityName] = entitySummaries[entityName] ?? [];
-				entitySummaries[entityName].push(summaryItem);
-				acc.updates = entitySummaries;
+				const entitySummaries = updates[entityName] ?? [];
+				entitySummaries.push(summaryItem);
+				updates[entityName] = entitySummaries;
 				break;
 			}
 			case 'DELETE': {
-				const entitySummaries = acc.deletes ?? {};
-				entitySummaries[entityName] = entitySummaries[entityName] ?? { recordsCount: 0, errors: 0 };
-				entitySummaries[entityName].recordsCount += totalRecords;
-				entitySummaries[entityName].errors += errors;
-				acc.deletes = entitySummaries;
+				const entitySummary = deletes[entityName] ?? { recordsCount: 0, errors: 0 };
+				entitySummary.recordsCount += rowTotalRecords;
+				entitySummary.errors += rowErrors;
+				deletes[entityName] = entitySummary;
 				break;
 			}
 		}
+	}
 
-		return acc;
-	}, initialSummary);
+	return {
+		inserts,
+		updates,
+		deletes,
+		totalRecords,
+		errors,
+	};
+};
+
+/**
+ * Utility to convert a raw Submission record to a Response type
+ * @param {SubmissionSummary} submission
+ * @returns {SubmissionSummaryResponse}
+ */
+export const createSubmissionSummaryResponse = (submission: SubmissionSummary): SubmissionSummaryResponse => {
+	return {
+		id: submission.id,
+		data: submission.data,
+		dictionary: submission.dictionary,
+		dictionaryCategory: submission.dictionaryCategory,
+		organization: submission.organization,
+		status: submission.status,
+		createdAt: _.toString(submission.createdAt?.toISOString()),
+		createdBy: _.toString(submission.createdBy),
+		updatedAt: _.toString(submission.updatedAt?.toISOString()),
+		updatedBy: _.toString(submission.updatedBy),
+	};
 };
