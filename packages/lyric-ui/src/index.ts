6a4c91d5df715ad09c721cf26313b6390734d959
James Lopez <jamestlopez.code@gmail.com>
2026-08-11 15:11:30 -0400

    fix: change button location to support shadcn components

--- packages/lyric-ui/src/index.ts
+++ packages/lyric-ui/src/index.ts
@@ -1,4 +1,4 @@
 import '@/style.css';
 
-export { Button, type ButtonProps, buttonVariants } from './components/button/button';
+export { Button, type ButtonProps, buttonVariants } from './components/ui/button';
 export { cn } from './lib/utils';
