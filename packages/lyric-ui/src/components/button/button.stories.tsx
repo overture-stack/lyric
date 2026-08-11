a33c78deb937cb43e729bd2935a6386032f8b7e3
James Lopez <jamestlopez.code@gmail.com>
2026-08-11 14:48:37 -0400

    fix: build issues

--- packages/lyric-ui/src/components/button/button.stories.tsx
+++ packages/lyric-ui/src/components/button/button.stories.tsx
@@ -1,6 +1,6 @@
 import type { Meta, StoryObj } from '@storybook/react';
 
-import { Button } from './Button.js';
+import { Button } from './button';
 
 const meta: Meta<typeof Button> = {
 	title: 'Components/Button',
--- packages/lyric-ui/src/index.ts
+++ packages/lyric-ui/src/index.ts
@@ -1,4 +1,4 @@
 import '@/style.css';
 
-export { Button, type ButtonProps, buttonVariants } from './components/button/Button';
+export { Button, type ButtonProps, buttonVariants } from './components/button/button';
 export { cn } from './lib/utils';
