b35ba5b7bcb7faf819abf349fa2f1e684f936fa5
James Lopez <jamestlopez.code@gmail.com>
2026-08-25 19:43:41 -0400

    fix: export components

--- packages/lyric-ui/src/index.ts
+++ packages/lyric-ui/src/index.ts
@@ -1,4 +1,31 @@
 import '@/style.css';
 
-export { Button, type ButtonProps, buttonVariants } from './components/ui/button';
+export { Button, buttonVariants } from './components/ui/button';
+export {
+	Combobox,
+	ComboboxChip,
+	ComboboxChips,
+	ComboboxChipsInput,
+	ComboboxCollection,
+	ComboboxContent,
+	ComboboxEmpty,
+	ComboboxGroup,
+	ComboboxInput,
+	ComboboxItem,
+	ComboboxLabel,
+	ComboboxList,
+	ComboboxSeparator,
+	ComboboxTrigger,
+	ComboboxValue,
+} from './components/ui/combobox/combobox';
+export { Input } from './components/ui/input/input';
+export {
+	InputGroup,
+	InputGroupAddon,
+	InputGroupButton,
+	InputGroupInput,
+	InputGroupText,
+	InputGroupTextarea,
+} from './components/ui/input/input-group';
+export { Textarea } from './components/ui/input/textarea';
 export { cn } from './lib/utils';
