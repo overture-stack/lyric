4ed8e1ccd85a7aeffe5a1544f84cd49f2c4cc7c7
James Lopez <jamestlopez.code@gmail.com>
2026-08-25 19:44:27 -0400

    fix: resolve exporting commonjs

--- packages/lyric-ui/vite.config.ts
+++ packages/lyric-ui/vite.config.ts
@@ -2,7 +2,7 @@ import { resolve } from 'node:path';
 
 import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
-import { defineConfig } from 'vite';
+import { defineConfig, esmExternalRequirePlugin } from 'vite';
 
 export default defineConfig({
 	plugins: [react(), tailwindcss()],
@@ -18,11 +18,15 @@ export default defineConfig({
 			fileName: 'lyric-ui',
 			formats: ['es', 'cjs'],
 		},
-		rollupOptions: {
-			external: ['react', 'react-dom', 'react/jsx-runtime'],
+		rolldownOptions: {
 			output: {
 				banner: "import './lyric-ui.css';",
 			},
+			plugins: [
+				esmExternalRequirePlugin({
+					external: ['react', 'react-dom', 'react/jsx-runtime'],
+				}),
+			],
 		},
 		cssCodeSplit: false,
 	},
